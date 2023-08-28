d12dce4034ffbaaf6cef69bc0bf844fdefbe781c
ayb <ayyubiy67@gmail.com>
2023-08-28 13:06:01 +0100

    edited the header and the navbar components

--- linkedin-clone/src/components/Header.tsx
+++ linkedin-clone/src/components/Header.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      <h1 className="text-xl font-semibold">Header</h1>
-      {/* Here you can add elements like logo, navigation links, etc. */}
-    </div>
+    <header className="bg-blue-800 text-white p-4">
+      <div className="container mx-auto flex items-center justify-between">
+        
+        {/* Logo Section */}
+        <div className="logo">
+          <h1 className="text-2xl font-bold">LinkedIn</h1>
+        </div>
+        
+        {/* Search Bar */}
+        <div className="search relative">
+          <input type="text" placeholder="Search" className="bg-white text-black p-2 rounded-full w-64" />
+          <span className="absolute top-3 left-3 text-gray-400">
+            🔍
+          </span>
+        </div>
+        
+        {/* Navigation Icons */}
+        <div className="nav-icons flex space-x-4">
+          <span className="cursor-pointer hover:bg-blue-700 p-2 rounded-full">🏠 Home</span>
+          <span className="cursor-pointer hover:bg-blue-700 p-2 rounded-full">👥 Network</span>
+          <span className="cursor-pointer hover:bg-blue-700 p-2 rounded-full">🔔 Notifications</span>
+          <span className="cursor-pointer hover:bg-blue-700 p-2 rounded-full">✉️ Messages</span>
+        </div>
+      </div>
+    </header>
   );
 };
 
--- linkedin-clone/src/components/Navbar.tsx
+++ linkedin-clone/src/components/Navbar.tsx
@@ -2,17 +2,42 @@ import React from 'react';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar__logo">
-        <img src="/path/to/your/logo.png" alt="LinkedIn Clone" />
+    <nav className="bg-blue-800 p-4 text-white">
+      <div className="container mx-auto flex items-center justify-between">
+        
+        {/* Logo Section */}
+        <div className="navbar__logo">
+          <img src="/path/to/your/logo.png" alt="LinkedIn Clone" className="w-10 h-10" />
+        </div>
+
+        {/* Search Bar */}
+        <div className="relative">
+          <input type="text" placeholder="Search" className="bg-white text-black p-2 rounded-full w-64" />
+          <span className="absolute top-2 left-2 text-gray-400">
+            🔍
+          </span>
+        </div>
+
+        {/* Nav Links */}
+        <ul className="flex space-x-6">
+          <li className="hover:bg-blue-700 p-2 rounded-full cursor-pointer">
+            🏠<br />Home
+          </li>
+          <li className="hover:bg-blue-700 p-2 rounded-full cursor-pointer">
+            👥<br />Network
+          </li>
+          <li className="hover:bg-blue-700 p-2 rounded-full cursor-pointer">
+            💼<br />Jobs
+          </li>
+          <li className="hover:bg-blue-700 p-2 rounded-full cursor-pointer">
+            💬<br />Messaging
+          </li>
+          <li className="hover:bg-blue-700 p-2 rounded-full cursor-pointer">
+            🙋<br />Me
+          </li>
+        </ul>
+
       </div>
-      <ul className="navbar__links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/network">My Network</a></li>
-        <li><a href="/jobs">Jobs</a></li>
-        <li><a href="/messaging">Messaging</a></li>
-        <li><a href="/profile">Me</a></li>
-      </ul>
     </nav>
   );
 };
